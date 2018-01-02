bb46291efe59e9f06a2565ce2fe1c8175471f61e
DanishKalim1 <engrdanishkalim@gmail.comgit config --global user.email engrdanishkalim@gmail.comgit config --global user.email engrdanishkalim@gmail.com>
2018-01-03 02:32:53 +0500

    Assignment 2.10 in this excercise i used object literal to dynamically add  title and images with javascript and used jquery mouseenter event

--- cf_template-2.3/js/javascript.js
+++ cf_template-2.3/js/javascript.js
@@ -71,20 +71,27 @@ $(".message-box").on("keyup", function() {
     };
 });
 //end of document.ready here
-
 //work section
 $(document).ready(function(){
   for(var i = 0; i < works.length; ++i ) {
     $("#work").append("\
-       <div class='col-sm-6 col-md-3'>\
-         <img class='img-responsive' src='" + works[i] + "'>\
-       </div>\
-  ");
-  var images = $("#work img");
+  <div class='col-sm-3 col-md-3'>\
+    <a href='" + works[i].url + "' class='work-img'>\
+      <img class='img-responsive' src='" + works[i].pic + "'>\
+       <span class='info title'>" + works[i].title + "</span>\
+    </a>\
+  </div>\
+");
+var images = $("#work img");
     if(i%2 === 0){
       $(images[i]).css("border", "2px solid DodgerBlue");
       } else {
       $(images[i]).css("border", "2px solid salmon");
     };
+    $(".work-img").mouseenter(function(){
+  $(".info", this).show();
+}).mouseleave(function(){
+  $(".info", this).hide();
+});
   };
 });
--- cf_template-2.3/js/work.js
+++ cf_template-2.3/js/work.js
@@ -1,6 +1,20 @@
- var works = [
- "http://www.animal-photography.com/thumbs/silver_tabby_kittens~AP-0JO6Y9-TH.jpg",
-  "http://www.animal-photography.com/thumbs/red_tabby_long_hair_kitten_~AP-0UJFTC-TH.jpg",
-  "http://www.animal-photography.com/thumbs/silver_tabby_kittens~AP-0JO6Y9-TH.jpg",
-  "http://www.animal-photography.com/thumbs/silver_tabby_kitten_looking_up~AP-0DLVMB-TH.jpg"
-]
+var works = [
+  {
+     title: "calulater layout",
+     pic: "img/calculate.png",
+     url:"http://stoic-bhabha-050da6.bitballoon.com/"
+  },
+  {
+    title: "Bootstrap site",
+    pic: "img/bootstrap.png",
+    url:"http://engineer-rehabilitation-30413.bitballoon.com/"
+  },
+  {
+    title: "Coming Soon...",
+    pic: "http://www.animal-photography.com/thumbs/silver_tabby_kittens~AP-0JO6Y9-TH.jpg"
+  },
+  {
+    title: "Coming Soon...",
+    pic: "http://www.animal-photography.com/thumbs/silver_tabby_kitten_looking_up~AP-0DLVMB-TH.jpg"
+  }
+];
